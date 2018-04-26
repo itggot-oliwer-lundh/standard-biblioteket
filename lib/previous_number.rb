23f834633556cf5dbf313a301ad7d5bd49e10e70
itggot-oliwer-lundh <38528588+itggot-oliwer-lundh@users.noreply.github.com>
2018-04-26 14:26:51 +0200

    Dokumenterade uppgiften

--- lib/previous_number.rb
+++ lib/previous_number.rb
@@ -1,3 +1,13 @@
+# Public: Takes an integer as input and returns the previous number as output.
+#
+# number  - The Integer that will be subtracted with "1". 
+#
+# Examples
+#
+#   previous_number(3)
+#   # => '2'
+#
+# Returns the previous number. 
 def previous_number(number)
     return number -1
 end
