ba800c57a0f91c14225e9c603655d9ecda327378
itggot-oliwer-lundh <38528588+itggot-oliwer-lundh@users.noreply.github.com>
2018-04-26 14:11:15 +0200

    Dokumenterade uppgiften

--- lib/between.rb
+++ lib/between.rb
@@ -1,3 +1,19 @@
+# Public: Takes three numbers as input and determines if the first one is between the second and third.
+#
+# n1  - The Integer to be compared if it is between the other two Integer. 
+# n2  - The Integer that needs to be smaller than n1.
+# n3  - The Integer that needs to be bigger than n1.
+
+# Examples
+#
+#   between(5, 10, 100)
+#   # => 'false'
+#
+#   between(10, 5, 100)
+#   # => 'true'
+#
+# Returns true if the first one is between the second and third.
+# Returns false if the first one is not between the second and third.
 def between(n1, n2, n3)
     if n1 < n2 
         return false
