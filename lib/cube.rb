3c688fbe6426ce225b07baf1f80304130e7b97f4
itggot-oliwer-lundh <38528588+itggot-oliwer-lundh@users.noreply.github.com>
2018-04-26 14:13:39 +0200

    Dokumenterade uppgiften

--- lib/cube.rb
+++ lib/cube.rb
@@ -1,3 +1,13 @@
+# Public: Takes an integer as input and gives the cube of the number as output.
+#
+# number  - The Integer that will be cubed 
+#
+# Examples
+#
+#   cube(5)
+#   # => '125'
+#
+# Returns the cubed Integer.
 def square(number)
     return number*number*number
 end
