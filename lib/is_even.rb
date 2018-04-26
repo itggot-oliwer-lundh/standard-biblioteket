2efba59cf23bf72e4c93a2f947906227e24540ea
itggot-oliwer-lundh <38528588+itggot-oliwer-lundh@users.noreply.github.com>
2018-04-26 14:16:38 +0200

    Dokumenterade uppgiften

--- lib/is_even.rb
+++ lib/is_even.rb
@@ -1,3 +1,17 @@
+# Public: Takes an integer as input and determine if the number is even
+#
+# number  - The Integer that will be decided if it is even. 
+#
+# Examples
+#
+#   is_even(5)
+#   # => 'false'
+#
+#   is_even(2)
+#   # => 'true'
+#
+# Returns true if the Integer is even.
+# Returns false if the Integer is not even.
 def is_even(number)
     if number % 2== 0
         return true
