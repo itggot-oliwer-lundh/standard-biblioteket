eb2c4f1f3185016f1f8ab606b2a503b09b9079e2
itggot-oliwer-lundh <38528588+itggot-oliwer-lundh@users.noreply.github.com>
2018-04-26 14:17:33 +0200

    Dokumenterade uppgiften

--- lib/is_negative.rb
+++ lib/is_negative.rb
@@ -1,3 +1,17 @@
+# Public: Takes an integer as input and determine if the number is negative
+#
+# number  - The Integer that will be decided if it is negative. 
+#
+# Examples
+#
+#   is_even(5)
+#   # => 'true'
+#
+#   is_even(2)
+#   # => 'false'
+#
+# Returns true if the Integer is negative.
+# Returns false if the Integer is not negative.
 def is_negative(number)
     if number < 0 
         return true
