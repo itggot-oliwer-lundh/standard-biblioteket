bb8f6e5c4551a87a9b92407f7521e6a0674a9936
itggot-oliwer-lundh <38528588+itggot-oliwer-lundh@users.noreply.github.com>
2018-04-26 14:27:33 +0200

    Dokumenterade uppgiften

--- lib/square.rb
+++ lib/square.rb
@@ -1,3 +1,13 @@
+# Public: Takes an integer as input and gives the square of the number as output.
+#
+# number  - The Integer that will be squared.  
+#
+# Examples
+#
+#   square(5)
+#   # => '25'
+#
+# Returns the squared Integer.
 def square(number)
     return number*number
 end
