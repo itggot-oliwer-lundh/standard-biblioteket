1b7a1fda33a662d1480ac2bc1b27a42bef06b795
itggot-oliwer-lundh <38528588+itggot-oliwer-lundh@users.noreply.github.com>
2018-04-26 14:25:46 +0200

    Dokumenterade uppgiften

--- lib/next_number.rb
+++ lib/next_number.rb
@@ -1,3 +1,13 @@
+# Public: Takes an integer as input and returns the next number as output.
+#
+# number  - The Integer that will be added with "1". 
+#
+# Examples
+#
+#   next_number(3)
+#   # => '4'
+#
+# Returns the next number. 
 def next_number(number)
     return number +1
 end
