dd90837168ce7f297a5fdd805dee7134048641ad
itggot-oliwer-lundh <38528588+itggot-oliwer-lundh@users.noreply.github.com>
2018-04-26 14:22:27 +0200

    Dokumenterade uppgiften

--- lib/min_of_two.rb
+++ lib/min_of_two.rb
@@ -1,3 +1,14 @@
+# Public: Takes two Integer as input and determine wich number is smallest.
+#
+# n1  - The first Integer that will be decided if it is the smallest. 
+# n2  - The second Integer that will be decided if it is the smallest. 
+#
+# Examples
+#
+#   min_of_two(5, 10)
+#   # => '5'
+#
+# Returns the smallest Integer
 def min_of_two(n1, n2)
     if n1 <= n2
         return n1
