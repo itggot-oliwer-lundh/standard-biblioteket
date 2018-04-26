80e879d2f7afd3862238f4ed92e0666c2f50c341
itggot-oliwer-lundh <38528588+itggot-oliwer-lundh@users.noreply.github.com>
2018-04-26 14:20:26 +0200

    Dokumenterade uppgiften

--- lib/is_odd.rb
+++ lib/is_odd.rb
@@ -1,3 +1,17 @@
+# Public: Takes an integer as input and determine if the number is odd.
+#
+# number  - The Integer that will be decided if it is odd. 
+#
+# Examples
+#
+#   is_odd(5)
+#   # => 'true'
+#
+#   is_odd(2)
+#   # => 'true'
+#
+# Returns true if the Integer is odd.
+# Returns false if the Integer is not odd.
 def is_odd(number)
     if number % 2== 1
         return true
