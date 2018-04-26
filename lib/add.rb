d327b6e61f82fb18747842bbb2f9def80bee0c31
itggot-oliwer-lundh <38528588+itggot-oliwer-lundh@users.noreply.github.com>
2018-04-26 13:58:37 +0200

    Dokumenterade uppgiften

--- lib/add.rb
+++ lib/add.rb
@@ -1,3 +1,15 @@
-def add (term1, term2)
+# Public: Takes two Integer as input and add them together as an output.
+#
+# term1  - The first Integer to be added.
+# term2  - The second Integer to be added.
+# 
+#
+# Examples
+#
+#   add(4, 2)
+#   # => '6'
+#
+# Returns the two Integer added together.
+def add(term1, term2)
     return term1 + term2
 end
