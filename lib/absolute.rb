c2cd73d6dc30ac6d90fada9779006691dd11fc92
itggot-oliwer-lundh <38528588+itggot-oliwer-lundh@users.noreply.github.com>
2018-04-26 13:54:43 +0200

    Dokumenerade uppgiften

--- lib/absolute.rb
+++ lib/absolute.rb
@@ -1,3 +1,14 @@
+# Public: Takes an Integer as input and gives the absolute value of the number as an output.
+#
+# number  - The Integer to be absolute.
+# 
+#
+# Examples
+#
+#   absolute(-200)
+#   # => '200'
+#
+# Returns the absolute Integer.
 def absolute(number)
     if number <0
         return number * -1
